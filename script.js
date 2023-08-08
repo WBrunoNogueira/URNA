55c67dafd1db68c39919cb51293405e652c71264
wds <wendersonbruno10@gmail.com>
2023-08-07 21:45:18 -0300

    criado variaveis de controle

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const s = (el) => document.querySelector(el);
+
+
+let seuVotoPara = s('.divisao-1-1 span');
+let cargo = s('.divisao-1-2 span');
+let descricao = s('.divisao-1-4');
+let aviso = s('.divisao-2');
+let lateral = s('.divisao-1-direita');
+let numeros = s('.divisao-1-3');
